60296e9d6ce6d9f61b5e451d28836502570a590d
Eddy Zhu <ezhu@dsny.nyc.gov>
2015-04-22 23:24:50 -0400

    added header enricher

--- cruiser-services/src/test/java/com/prudent/office/file/service/FileServiceApplicationTest.java
+++ cruiser-services/src/test/java/com/prudent/office/file/service/FileServiceApplicationTest.java
@@ -1,14 +1,16 @@
 package com.prudent.office.file.service;
 
-import org.junit.Assert;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.channel.DirectChannel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,20 +18,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes=FileServiceApplication.class)
 public class FileServiceApplicationTest {
 	@Autowired
-	@Qualifier("files")
-	private DirectChannel files;
+	@Qualifier("filesFiles")
+	private MessageChannel files;
 	
 	@Autowired
-	@Qualifier("output")
-	private DirectChannel output;
+	@Qualifier("stamped")
+	private PollableChannel output;
 	
+	@Value("#{ systemProperties['user.name'] }")
+	private String systemUserName;
+	
+	String fileName = "/tmp/si/test1.txt";
 	@Test
 	public void gotFile() {
-		Assert.assertTrue(files.send(createTextMessage()));
+		files.send(createTextMessage());
+		Message<?> out = output.receive();
+		assertThat(out.getHeaders().get("sender"), is(systemUserName));
+		assertThat(out.getPayload(), is(fileName));
 	}
 
-	
 	Message<?> createTextMessage(){
-		return MessageBuilder.withPayload("/tmp/si/test1.txt").build();
+		return MessageBuilder.withPayload(fileName).build();
 	}
 }
