0b09e64df11fea3632a0f9d97ffd8f193fd00fd2
eddydsny <ezhu@dsny.nyc.gov>
2015-04-23 14:32:35 -0400

    check in setup

---
+++ cruiser-data/src/main/java/com/prudent/office/data/gf/GFMain.java
@@ -0,0 +1,17 @@
+package com.prudent.office.data.gf;
+
+import com.gemstone.gemfire.cache.Cache;
+import com.gemstone.gemfire.cache.CacheFactory;
+
+public class GFMain {
+
+	public static void main(String[] args) {
+		Cache cache = new CacheFactory()
+			.set("locators", "localhost[55221]")
+			.set("mcast-port", "0")
+			.set("log-level", "error")
+			.create();
+		System.out.println();
+	}
+
+}
