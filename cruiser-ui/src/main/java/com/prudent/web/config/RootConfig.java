135eabd31a3fd9ca3c1570ddbf1763b2e882f681
Eddy Zhu <ezhu@dsny.nyc.gov>
2015-04-25 17:31:05 -0400

    added web module

---
+++ cruiser-ui/src/main/java/com/prudent/web/config/RootConfig.java
@@ -0,0 +1,7 @@
+package com.prudent.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+}
---
+++ cruiser-ui/src/main/java/com/prudent/web/config/SpitterWebInitializer.java
@@ -0,0 +1,24 @@
+package com.prudent.web.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.prudent.web.WebConfig;
+
+public class SpitterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+  
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class<?>[] { RootConfig.class };
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class<?>[] { WebConfig.class };
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] { "/" };
+  }
+
+}
